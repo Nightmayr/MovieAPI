b944688eee9a825ce0facad712a6e6e685e9bb32
Umayr <umayr23@gmail.com>
2019-01-04 13:33:59 +0000

    Added FilmRepository to persistence.repopackage

--- src/main/java/com/qa/business/service/FilmServiceImplementation.java
+++ src/main/java/com/qa/business/service/FilmServiceImplementation.java
@@ -1,5 +1,32 @@
 package com.qa.business.service;
 
-public class FilmServiceImplementation {
+import javax.inject.Inject;
 
+import org.apache.log4j.Logger;
+
+import com.qa.persistence.repository.FilmRepository;
+
+public class FilmServiceImplementation implements FilmService {
+
+
+	@Inject
+	private FilmRepository repo;
+	
+
+	public String getAllFilms() {
+		return repo.getAllFilms();
+	}
+
+	public String addFilm(String film) {
+		return repo.addFilm(film);
+	}
+
+	public String deleteFilm(String name) {
+		return repo.deleteFilm(name);
+	}
+	
+	public void setRepo(FilmRepository repo) {
+		this.repo = repo;
+	}
+	
 }
--- src/main/java/com/qa/persistence/repository/FilmDBRepository.java
+++ src/main/java/com/qa/persistence/repository/FilmDBRepository.java
@@ -1,14 +1,38 @@
 package com.qa.persistence.repository;
 
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
 
+import java.util.Collection;
 
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+
+import com.qa.persistence.domain.Film;
+import com.qa.util.JSONUtil;
+
+@Transactional(SUPPORTS)
+@Default
 public class FilmDBRepository implements FilmRepository {
 
+	@PersistenceContext(unitName = "primary")
+	private EntityManager manager;
+	
+	@Inject
+	private JSONUtil util;
+	
 	public String getAllFilms() {
-		// TODO Auto-generated method stub
-		return null;
+		Query query = manager.createQuery("SELECT * FROM Film");
+		Collection<Film> films = (Collection<Film>)query.getResultList();
+		return util.getJSONForObject(films);
 	}
-
+	
+	
 	public String addFilm(String film) {
 		// TODO Auto-generated method stub
 		return null;
